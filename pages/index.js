6a47fab4c55891e15e7ed321a088b26610b3591e
“Kimberly <kchaparro@ucdavis.edu>
2023-11-08 19:24:01 -0800

    updates here

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,25 @@ import { getSortedList } from '../lib/data';
 
 // define a getStaticProps() function - this name is defined by next.js
 export async function getStaticProps() {
-  const allData = getSortedList();
+  const allData = await getSortedList();
   return {
     props: { allData }
   };
 }
 
 // export our home page component Home
-export default function Home( { allData } ) {
+export default function Home({ allData }) {
   return (
     <Layout home>
-      <h4 className = "mb-3 text-center">(by a dessert-enthusiast)</h4>
+      <h4 className="mb-3 text-center">(by a dessert-enthusiast)</h4>
       <div className="list-group">
         {allData.map(
-            ({id, name}) => (
-              <Link key={id} href={`/${id}`} className="list-group-item list-group-item-action">
-                {name}
-              </Link>
-            )
+          ({ id, name }) => (
+            <Link key={id} href={`/${id}`} className="list-group-item list-group-item-action">
+              {name}
+            </Link>
           )
+        )
         }
       </div>
     </Layout>
