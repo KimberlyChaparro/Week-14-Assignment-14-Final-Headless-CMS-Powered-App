f2408adfa96f53ca04d5516d0aae394813a1f5ca
“Kimberly <kchaparro@ucdavis.edu>
2023-11-28 18:30:21 -0800

    functionality

--- pages/index.js
+++ pages/index.js
@@ -5,29 +5,33 @@ import { getSortedList } from '../lib/data';
 import { getSortedPurchasesList } from '../lib/purchases_data';
 import { getSortedContactsList } from '../lib/contacts_data';
 import { getSortedRecipientsList } from '../lib/recipients_data';
+import { getSortedProductsList } from '../lib/products_data';
 
 export async function getStaticProps() {
   const allData = await getSortedList();
   const allContactsData = await getSortedContactsList();
   const allPurchasesData = await getSortedPurchasesList();
   const allRecipientsData = await getSortedRecipientsList();
+  const allProductsData = await getSortedProductsList();
   return {
     props: {
       allData,
       allContactsData,
       allPurchasesData,
-      allRecipientsData
+      allRecipientsData,
+      allProductsData
     }
   };
 }
-export default function Home({ allData, allContactsData, allPurchasesData, allRecipientsData }) {
+export default function Home({ allData, allContactsData, allPurchasesData, allRecipientsData, allProductsData }) {
   return (
+
     <Layout home>
-      <h3>List of All Posts</h3>
+      <h3>List of Products</h3>
       <div className="list-group">
-        {allData.map(
+        {allProductsData.map(
           ({ id, name }) => (
-            <Link key={id} href={`/posts/${id}`} className="list-group-item list-group-item-action">
+            <Link key={id} href={`/products/${id}`} className="list-group-item list-group-item-action">
               {name}
             </Link>
           )
@@ -61,6 +65,17 @@ export default function Home({ allData, allContactsData, allPurchasesData, allRe
         {allRecipientsData.map(
           ({ id, name }) => (
             <Link key={id} href={`/recipients/${id}`} className="list-group-item list-group-item-action">
+              {name}
+            </Link>
+          )
+        )
+        }
+      </div>
+      <h3>List of All Posts</h3>
+      <div className="list-group">
+        {allData.map(
+          ({ id, name }) => (
+            <Link key={id} href={`/posts/${id}`} className="list-group-item list-group-item-action">
               {name}
             </Link>
           )
